70b544b67ca1d56a05719abe0b0f899130ff5bf6
Pratik Prajapati <mail.petrickhelp@gmail.com>
2021-04-25 12:58:40 +0530

    Initial Commit

---
+++ HealthyFood/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HealthyFood
+//
+//  Created by Pratikkumar Prajapati on 25/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
